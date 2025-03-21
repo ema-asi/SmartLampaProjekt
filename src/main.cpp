93affc6e3a28594086fb7bd07b709c730a55c5c0
NewNamesAreHard <oscar.asserlund@gmail.com>
2025-03-21 09:35:48 +0100

    Many minor changes, mainly pushing project forward towards MVP status

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ extern "C" char *sbrk(int i);
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 ClapDetection clapdetection(SampleSize, Sound_Treshold, ClapWindow);
-bool isLampOn = true;
+bool isLampOn = false;
 int brightness = 0;
 
 // Function Declarations:
@@ -37,46 +37,46 @@ void reconnectToWiFi();
 
 void setup()
 {
-  Serial.begin(115200);
-  lcd.begin(16, 2); // Sets up the display, defining number or rows(Y) and columns(X)
+    Serial.begin(115200);
+    lcd.begin(16, 2); // Sets up the display, defining number or rows(Y) and columns(X)
 
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(PhotoResistor_PIN, INPUT);
-  pinMode(SoundAnalog_PIN, INPUT);
-  pinMode(SoundDigital_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(PhotoResistor_PIN, INPUT);
+    pinMode(SoundAnalog_PIN, INPUT);
+    pinMode(SoundDigital_PIN, INPUT);
 
-  ConnectToWifi();
+    // WiFi Related Setup
+    lcd.println("Connecting to WiFi!");
+    ConnectToWifi();
+    lcd.clear();
+    lcd.println("Established connection!");
 }
 
 void loop()
 {
-  reconnectToWiFi();
+    reconnectToWiFi();
 
-  Serial.print("Light Intensity: ");
-  Serial.println(analogRead(PhotoResistor_PIN));
-  Serial.print("Sound Intensity: ");
-  Serial.println(analogRead(SoundAnalog_PIN));
+    // Serial.print("Light Intensity: ");               // Debugging message
+    // Serial.println(analogRead(PhotoResistor_PIN));   // Debugging message
+    // Serial.print("Sound Intensity: ");               // Debugging message
+    // Serial.println(analogRead(SoundAnalog_PIN));     // Debugging message
 
-  brightness = lightSensorAverageReading();
+    brightness = lightSensorAverageReading();
 
-  lightOnClaps();
-  setBrightness();
+    lightOnClaps();
+    setBrightness();
 
-  // Example usage of LCD-functionality:
-  lcd.print("Hello World!");
-
-  delay(100);
+    delay(100);
 }
 
 void reconnectToWiFi()
 {
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    ConnectToWifi();
-  }
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        ConnectToWifi();
+    }
 }
 
-// Proposed replacement for above code
 /**
  * @brief Reads the light sensor and calculates the average brightness value.
  *
@@ -87,64 +87,67 @@ void reconnectToWiFi()
  */
 int lightSensorAverageReading()
 {
-  const int sizeOfArray = 20;                             // Size of the array to store brightness values
-  static std::array<int, sizeOfArray> brightnessValues{}; // Array to store the last 5 brightness values
-  static int8_t index = 0;                                // Index to keep track of the current position in the array
-  int sum{};                                              // Variable to store the sum of brightness values
+    const int sizeOfArray = 20;                             // Size of the array to store brightness values
+    static std::array<int, sizeOfArray> brightnessValues{}; // Array to store the last 5 brightness values
+    static int8_t index = 0;                                // Index to keep track of the current position in the array
+    int sum{};                                              // Variable to store the sum of brightness values
 
-  int sensor_input = analogRead(PhotoResistor_PIN); // Read the current value from the light sensor
+    int sensor_input = analogRead(PhotoResistor_PIN); // Read the current value from the light sensor
 
-  // Scale the sensor input to a range of 0-255
-  int scaled_input = (sensor_input - Light_Dark_Value) * 255 / (Light_Bright_Value - Light_Dark_Value);
+    // Scale the sensor input to a range of 0-255
+    int scaled_input = (sensor_input - Light_Dark_Value) * 255 / (Light_Bright_Value - Light_Dark_Value);
 
-  // Constrain the scaled input to be within the range 0-255
-  scaled_input = (scaled_input > 255) ? 255 : (scaled_input < 0) ? 0
-                                                                 : scaled_input;
+    // Constrain the scaled input to be within the range 0-255
+    scaled_input = (scaled_input > 255) ? 255 : (scaled_input < 0) ? 0 : scaled_input;
 
-  brightnessValues[index] = scaled_input; // Store the scaled input in the array
-  index = (index + 1) % sizeOfArray;      // Increment the index and wrap around if necessary
+    brightnessValues[index] = scaled_input; // Store the scaled input in the array
+    index = (index + 1) % sizeOfArray;      // Increment the index and wrap around if necessary
 
-  for (const int &elements : brightnessValues) // Calculate the sum of the brightness values in the array
-  {
-    sum += elements;
-  }
+    for (const int &elements : brightnessValues) // Calculate the sum of the brightness values in the array
+    {
+        sum += elements;
+    }
 
-  return (sum / sizeOfArray); // Return the average brightness value
+    return (sum / sizeOfArray); // Calculate and then return the average brightness value
 }
 
 void setBrightness()
 {
-  if (isLampOn)
-  {
-    analogWrite(LED_PIN, lightSensorAverageReading()); // Proposed solution
-  }
-  else
-  {
-    analogWrite(LED_PIN, 0); // Set LED brightness, send PWM signal to LED
-  }
+    if (isLampOn)
+    {
+        analogWrite(LED_PIN, lightSensorAverageReading()); // Proposed solution
+    }
+    else
+    {
+        analogWrite(LED_PIN, 0); // Set LED brightness to 0
+    }
 }
 
 void lightOnClaps()
 {
-  if (clapdetection.detect_claps(SoundAnalog_PIN))
-  {
-    Serial.println("Clapdetection says: Beep Boop, you wake the computah!"); // Debugging message
+    if (clapdetection.detect_claps(SoundAnalog_PIN))
+    {
+    // Serial.println("Clapdetection says: Beep Boop, you wake the computah!"); // Debugging message that can be turned on
     isLampOn = !isLampOn;
-    setBrightness(); // Make sure the lamp is on/off without delay
-    delay(5000);
-  }
+    if (isLampOn)
+    {
+        setBrightness();
+    }
+    // setBrightness(); // Make sure the lamp is on/off without delay
+    // delay(5000);  // Commenting out this as we've agreed to not have functions call on delays
+    }
 }
 
 // This code shows the remaining free memory between the heap and the stack
 int getFreeMemory()
 {
-  char top;
-  return &top - sbrk(0);
+    char top;
+    return &top - sbrk(0);
 }
 
 void printFreeMemory()
 {
-  Serial.print("Free Memory: ");
-  Serial.print(getFreeMemory());
-  Serial.println(" Bytes.");
+    Serial.print("Free Memory: ");
+    Serial.print(getFreeMemory());
+    Serial.println(" Bytes.");
 }
