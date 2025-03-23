b3e8354a212028c65d9b189382f0ddca20674554
Jessica Sjösten <jessica.e.sjosten@gmail.com>
2025-03-23 21:55:57 +0100

    move code from loop to lightSensorAverageReading, changed light threasholds
    Co-authored-by: NewNamesAreHard <NewNamesAreHard@users.noreply.github.com>

--- src/arduino_secrets.h
+++ src/arduino_secrets.h
@@ -5,7 +5,7 @@
 // const char SSID[] = "SSID_NAME_HERE";           // your WiFi network name
 // const char PASSWORD[] = "WIFI_PASSWORD_HERE";   // your WiFi password
 
-const char SSID[] = "TP-Link_2928"; // your WiFi network name
-const char PASSWORD[] = "53849816"; // your WiFi password
+const char SSID[] = "Asus_5G";      // your WiFi network name
+const char PASSWORD[] = "7geazavp"; // your WiFi password
 
 #endif
